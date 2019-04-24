483b76cfc0b096b2fd201609975b58a696d3bbc4
ztt <37802239+ZTTZX@users.noreply.github.com>
2019-04-24 22:06:52 +0800

    Add files via upload

---
+++ smartdoc.py
@@ -0,0 +1,5 @@
+﻿import webbrowser
+file_code_addr = "C:\\Users\\admin\\Documents\\SmartDoc\\code.py"
+file_srs_addr = "C:\\Users\\admin\\Documents\\SmartDoc\\srs.txt"
+gen_code_html = "code.html"
+gen_srs_html = "srs.html"
