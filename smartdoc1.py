a7ee0a21a3db36364eff21f9621446ec9895e666
ztt <37802239+ZTTZX@users.noreply.github.com>
2019-05-22 22:34:02 +0800

    Add files via upload

--- smartdoc1.py
+++ smartdoc1.py
@@ -1,38 +1,61 @@
 import sys,re
+sys.setrecursionlimit(1000000)
 
 def lines(file):
-    """
-    生成器,在文本最后加一空行
-    """
-    for line in file: yield line
-    yield '\n'
- 
-def blocks(file):
-    """
-    生成器,生成单独的文本块
-    """
-    block = []
-    for line in lines(file):
-        if line.strip():
-            block.append(line)
-        elif block:
-            yield ''.join(block).strip()
-            block = []
+#     """
+#     生成器,在文本最后加一空行
+#     """
+#
+     for line in file: yield line
+     yield '\n'
+    
+     # for line in lines(file):
+     #     if line.strip():
+     #         line.append(line)
+     #     elif line:
+     #         yield ''.join(line).strip()
 
-
-print ('<html><body>')
-
+print('<html><body>')
+        
 title = True
-for block in blocks(sys.stdin):
-    block = re.sub(r'\*(.+?)\*',r'<em>\1</em>',block)
+for line in lines(sys.stdin):
+    line = re.sub(r'\*(.+?)\*',r'<em>\1</em>',line)
     if title:
-        print('<p>')
-        print (block)
-        print ('</p>')
-        title =False
+         print('<p>')
+         print (line)
+         print ('</p>')
+         title =False
     else:
-        print('<p>')
-        print (block)
-        print('</p>')
+         print('<p>')
+         print (line)
+         print('</p>')
+           
+def update_code(file):
+    file_data = ''
+    #new_str = 'href="www.baidu.com"'
+    with open(file, "r") as f:
+        for line in f:
+            if line.find("#{see rq")!=-1:
+                id_name = re.findall(("rq"+r"\d"),line)
+                if id_name[0] in line:
+                    old_str = "href='srs.html#"+id_name[0]+"'"
+                    # if not id_name[0] in srs:
+                    #     line = line.replace(old_str,new_str)
+            if line.find("#{see ra")!=-1:
+                id_name = re.findall(("ra"+r"\d"),line)
+                if id_name[0] in line:
+                    old_str = "href='srs.html#"+id_name[0]+"'"
+                    # if not id_name[0] in srs:
+                    #     line = line.replace(old_str,new_str)
+            if line.find("#{see tc")!=-1:
+                id_name = re.findall(("tc"+r"\d"),line)
+                if id_name[0] in line:
+                    old_str = "href='srs.html#"+id_name[0]+"'"
+                    # if not id_name[0] in srs:
+                    #     line = line.replace(old_str,new_str)
+            file_data += line
+    with open(file,"w") as f:
+        f.write(file_data)
 
-print('</body></html>')
+print('</html></body>')
+   
